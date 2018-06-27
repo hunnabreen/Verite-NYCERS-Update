ddce6f2dde5bbf69841171f4a910018bc95d1830
Hunter Breen <35741316+hunnabreen@users.noreply.github.com>
2018-06-27 14:36:26 -0400

    formatting/debugging
    test to navigate docx packages

--- nycers_update.py
+++ nycers_update.py
@@ -1,37 +1,46 @@
-#import document template from docx
-from docx import Document
-#import highlight colors
-from docx.enum.text import WD_COLOR_INDEX
+#import docx
+from docx import *
 #import os for splitting file types
 import os
 
-#init original document
-file = input("Enter file here: ")
-document = docx.Document(file)
+def main():
+    #init original document
+    file = test.docx #input("Enter file here: ")
+    document = docx.Document(file)
 
-#remove .docx file extension and save as UPDATED.docx
-file = os.path.splitext(file)[0] + '-UPDATE.docx'
-document.save(file)
+    open(document)
+    line = document.readlines
+    print(line)
+    
 
-"""
-    parses the document to update highlights
+    """
+        parses the document to update highlights
 
-    red => purple
-    green => blue
-    purple => delete
-    blue => unhighlight
+        red => purple
+        green => blue
+        purple => delete
+        blue => unhighlight
 
-"""
+    """
+'''
+    def textUpdate(filename):
+        doc = docx.Document(filename)
+        for chars in doc.paragraphs:
+            for run in chars.runs:
+                highlight = chars.text.highlight_color
+                if highlight == WD_COLOR_INDEX.RED:
+                    highlight = WD_COLOR_INDEX.VIOLET
+                elif highlight == WD_COLOR_INDEX.GREEN:
+                    highlight = WD_COLOR_INDEX.BLUE
+                elif highlight == WD_COLOR_INDEX.VIOLET:
+                    highlight = delete #delete
+                else:
+                    highlight = None
 
-def textUpdate(filename):
-    doc = docx.Document(filename)
-    for chars in doc.paragraphs:
-        highlight = chars.text.highlight_color
-        if highlight == WD_COLOR_INDEX.RED:
-            highlight = WD_COLOR_INDEX.VIOLET
-        elif highlight == WD_COLOR_INDEX.GREEN:
-            highlight = WD_COLOR_INDEX.BLUE
-        elif highlight == WD_COLOR_INDEX.VIOLET:
-            highlight = delete #delete
-        else:
-            highlight = WD_COLOR_INDEX.WHITE
+    #driver
+    textUpdate(document)
+    #remove .docx file extension and save as UPDATED.docx
+    file = os.path.splitext(file)[0] + '-UPDATE.docx'
+    document.save(file)
+'''
+main()
